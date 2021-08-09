c9d899cabb9e5640f8752a228a640bbef8ff2f34
Pralit Patel <pralit.patel@pnnl.gov>
2021-08-09 09:37:18 -0400

    Fix git tag and push commands

--- Mac/make-release.sh
+++ Mac/make-release.sh
@@ -7,7 +7,10 @@ cd $WORKSPACE
 
 # git remote add stash https://stash.pnnl.gov/scm/jgcri/gcam-core.git
 # git pull stash master
-# git tag add gcam-v${GCAM_VERSION}
+# git tag gcam-v${GCAM_VERSION}
+# git push stash gcam-v${GCAM_VERSION}
+# git push origin master
+# git push origin gcam-v${GCAM_VERSION}
 
 # Clean exe
 rm -rf input/gcamdata/.drake
