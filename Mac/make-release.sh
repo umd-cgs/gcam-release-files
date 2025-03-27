5c5be33b5ae9d8edcd353aaa6ae5e16dcff15607
Andy M <andy.miller331@gmail.com>
2025-03-27 13:21:19 -0400

    Improve documentation, mirror Windows script changes in Mac script

--- Mac/make-release.sh
+++ Mac/make-release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Define WORKSPACE path relative to location of make-release.sh 
 WORKSPACE=~/model/gcam-github
+# Define RELEASE_FILES path relative to WORKSPACE
 RELEASE_FILES=~/model/gcam-release-files
 GCAM_VERSION='7.0'
 cd $WORKSPACE
@@ -15,11 +17,14 @@ cd $WORKSPACE
 # Clean exe
 rm -rf input/gcamdata/.drake
 rm -rf input/gcamdata/renv/library
+rm -rf input/gcamdata/.Rproj.user/
+rm -rf input/gcamdata/outputs/
 rm -f exe/debug*
 rm -f exe/logs/*
 rm -f exe/restart/*
 cp exe/configuration_ref.xml exe/configuration.xml
 touch exe/.basexhome
+rm -f ModelInterface/logs/*
 
 cp "${RELEASE_FILES}/Mac/run-gcam.command" ./exe/
 cp -r "${RELEASE_FILES}/Additional Licenses" ./
@@ -30,9 +35,9 @@ cp -r "${RELEASE_FILES}/Additional Licenses" ./
 # MACOSX_DEPLOYMENT_TARGET = 10.9
 # Double check file list
 rm -f file_list_expanded
-IFS=$'\n'
+IFS=$'\r\n'
 for f in `cat ${RELEASE_FILES}/Mac/mac_files`; do find $f -type f | grep -v '.basex$' >> file_list_expanded; find $f -type l >> file_list_expanded; done
 unset IFS
 echo 'libs/java' >> file_list_expanded
 # TODO: automate checks to ensure no proprietary data
-zip -y gcam-v${GCAM_VERSION}-Mac-Release-Package.zip -@ < file_list_expanded
+zip gcam-v${GCAM_VERSION}-Mac-Release-Package.zip -@ < file_list_expanded
