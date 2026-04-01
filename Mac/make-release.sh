f19e0236ce51e6a84dba40063aa87a7b0854443f
Matthew Gidden <matthew.gidden@gmail.com>
2026-04-01 11:17:00 -0400

    update for mac release using makefiles

--- Mac/make-release.sh
+++ Mac/make-release.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Define WORKSPACE path absolutely or relative to location of make-release.sh 
-WORKSPACE='../../g-c_for_v7_Mac_arm64_build'
+WORKSPACE='../../gcam-china-dev'
 # Define RELEASE_FILES path absolutely or relative to WORKSPACE
 RELEASE_FILES='../gcam-release-files'
-GCAM_VERSION='8'
-cd $WORKSPACE
+RELEASE_VERSION_PATH='../releases/gcam-v8.2-Mac-Release-Package/'
 
 # git remote add stash https://stash.pnnl.gov/scm/jgcri/gcam-core.git
 # git pull stash master
@@ -13,6 +12,14 @@ cd $WORKSPACE
 # git push stash gcam-v${GCAM_VERSION}
 # git push origin master
 # git push origin gcam-v${GCAM_VERSION}
+
+# move to workspace
+cd $WORKSPACE
+
+# Copy over lib files
+mkdir -p ./libs
+cp -r $RELEASE_VERSION_PATH/libs/* ./libs/
+cp -r $RELEASE_VERSION_PATH/ModelInterface/* ./ModelInterface/
 
 # Clean exe
 rm -rf input/gcamdata/.drake
@@ -41,4 +48,4 @@ for f in `cat ${RELEASE_FILES}/Mac/mac_files`; do find $f -type f | grep -v '.ba
 unset IFS
 echo 'libs/java' >> file_list_expanded
 # TODO: automate checks to ensure no proprietary data
-zip gcam-china-v${GCAM_VERSION}-Mac_arm64-Release-Package.zip -@ < file_list_expanded
+#zip gcam-china-v${GCAM_VERSION}-Mac_arm64-Release-Package.zip -@ < file_list_expanded
